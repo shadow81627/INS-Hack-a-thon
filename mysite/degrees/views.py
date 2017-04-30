c63947050073904b2aa7a5d978d927acf0228208
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-04-30 10:59:26 +1000

    Fixed the tooltips

--- mysite/degrees/views.py
+++ mysite/degrees/views.py
@@ -6,8 +6,10 @@ from .models import Course
 
 def index(request):
     course_list = Course.objects.all()
-    suggest_list = Course.objects.order_by('code')[:12]
-    context = {'course_list': course_list, 'suggest_list': suggest_list}
+    suggest1_list = Course.objects.order_by('?')[:4]
+    suggest2_list = Course.objects.order_by('?')[:4]
+    suggest3_list = Course.objects.order_by('?')[:4]
+    context = {'course_list': course_list, 'suggest1_list': suggest1_list, 'suggest2_list': suggest2_list, 'suggest3_list': suggest3_list,}
     return render(request, 'degrees/index.html', context)
 
 
